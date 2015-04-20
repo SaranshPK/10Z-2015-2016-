4f03af53d4daaa5207c65943a79ac37082aa34a4
SaranshPK <saranshis@hotmail.com>
2015-04-19 20:48:41 -0700

    Useless
    Got rid of testing stuff

--- PushBot.c
+++ PushBot.c
@@ -27,8 +27,6 @@ void pre_auton()
   // Set bStopTasksBetweenModes to false if you want to keep user created tasks running between
   // Autonomous and Tele-Op modes. You will need to manage all user created tasks if set to false.
   bStopTasksBetweenModes = true;
-	bool meep = true;
-	bool derp = false;
 	// All activities that occur before the competition starts
 	// Example: clearing encoders, setting servo positions, ...
 }
